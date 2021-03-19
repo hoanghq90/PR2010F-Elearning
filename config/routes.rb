c344ebf09068d2c7d7aea4def720ebd2d88fad19
Jeremie <wucututa@gmail.com>
2021-03-19 10:39:48 +0700

    login-logoutv2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/index'
+  get 'students/show'
+  devise_for :students, controllers: { omniauth_callbacks: "omniauth_callbacks" }
+  resources :students, only: [:show]
+  root 'static_pages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
