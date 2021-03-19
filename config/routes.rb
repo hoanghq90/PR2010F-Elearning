e8140cfa877ffc86fafee56e5300b8bb1bfcbd1a
hoanghq90 <hoangts140190@gmail.com>
2021-03-19 10:52:33 +0700

    admin_devise_ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/index'
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  devise_for :students
+  root 'static_pages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
