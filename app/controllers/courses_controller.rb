c30ab50d7193fb43b6a6eb19def5e6be398c3cb6
hoanghq90 <hoangts140190@gmail.com>
2021-03-23 09:57:00 +0700

    cate_complete

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,16 +1,33 @@
 class CoursesController < ApplicationController
   before_action :find_course, only: [:show]
+
   def index
-   @courses = Course.paginate(page: params[:page]).per_page(4)
+     @categories = Category.all
+     if params[:cate]
+       category = Category.find_by id:  params[:cate]
+         if category.present?
+           @courses = category.courses
+         else
+           flash[:success] = "Category doesn't exist"
+           redirect_to categories_path
+         end
+     else
+       @courses = Course.all
+     end
   end
+
   def show
    @questions = @course.questions
   end
+
+ private
+
   def find_course
     @course =  Course.find_by id: params[:id]
     unless @course.present?
       flash[:success] = "Course doesn't exist"
-      redirect_to courses_url
+      redirect_to courses_path
     end
   end
+
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,5 @@ class Course < ApplicationRecord
   has_many :questions
   has_many :answers, through: :questions
   has_many :tests
+  belongs_to :category
 end
