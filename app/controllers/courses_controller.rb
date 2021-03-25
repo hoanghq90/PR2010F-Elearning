a8aa959a935ae8886d8cf15eadc6cb7318a88452
hoanghq90 <hoangts140190@gmail.com>
2021-03-25 13:37:38 +0700

    category2

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,11 +1,20 @@
 class CoursesController < ApplicationController
   before_action :find_course, only: [:show]
   def index
-   @courses = Course.paginate(page: params[:page]).per_page(4)
-  end
+    @categories = Category.all
+
+    cate = params[:cate]
+    if !cate.nil?
+      @courses = Course.where(:category_id => cate)
+    else
+       @courses = Course.all
+       # @courses = Course.paginate(page: params[:page]).per_page(4)
+   end
+ end
   def show
    @questions = @course.questions
   end
+
   def find_course
     @course =  Course.find_by id: params[:id]
     unless @course.present?
@@ -13,4 +22,5 @@ class CoursesController < ApplicationController
       redirect_to courses_url
     end
   end
+
 end
