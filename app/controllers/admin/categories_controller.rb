5b76ba1abbe3996537dd4e9351644c808cd6fb85
hoanghq90 <hoangts140190@gmail.com>
2021-03-26 15:59:43 +0700

    add_all

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,2 +1,54 @@
 class Admin::CategoriesController < ApplicationController
+  before_action :find_category, only: %i[ show edit update destroy ]
+
+  def index
+    @categories = Category.all
+  end
+
+  def show
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def edit
+  end
+
+  def create
+    @category = Category.new(category_params)
+      if @category.save
+        flash[:success] = "Create successfully category"
+        redirect_to admin_categories_path
+      else
+        render :new
+        flash[:danger] = 'Error'
+      end
+    end
+
+    def update
+		if @category.update(category_params)
+      flash[:success] = "Category updated"
+      redirect_to admin_categories_path
+		else
+			render 'edit'
+		end
+	end
+
+
+  def destroy
+   @category.destroy
+   flash[:success] = 'Category deleted'
+   redirect_to admin_categories_path
+ end
+
+  private
+
+    def find_category
+      @category = Category.find(params[:id])
+    end
+
+    def category_params
+      params.require(:category).permit(:category)
+    end
 end
